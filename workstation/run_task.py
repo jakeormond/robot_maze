2d008d59a86d1d9981f8c97388ac298e8114ae4d
Jake Ormond <j.ormond@ucl.ac.uk>
2024-05-30 17:27:13 +0100

    minor changes

--- workstation/run_task.py
+++ workstation/run_task.py
@@ -23,7 +23,7 @@ plt.ion
 
 # CONSTANTS
 min_platform_dura_new = 0.2  # minimum duration animal must be on new platform to register choice
-min_platform_dura_verify = 0.5  # minimum duration animal must be on  platform to verify choice
+# min_platform_dura_verify = 0.5  # minimum duration animal must be on  platform to verify choice
                             # after robots have made their initial turns 
 
 # top level folder
